c2b66cfef978e03bce5262b618d9e6da663c0a15
Wanthanee Pansrisuk <pa.wanthanee@gmail.com>
2020-05-11 22:22:48 +0700

    no message

--- bot.php
+++ bot.php
@@ -28,22 +28,38 @@ function send_reply_message($url, $post_header, $post_body)
     return $result;
 }
 
+// if ( sizeof($request_array['events']) > 0 ) {
+//       foreach ($request_array['events'] as $event) {
+      
+//       $reply_message = '';
+//       $reply_token = $event['replyToken'];
+//       $data = [
+//          'replyToken' => $reply_token,
+//          'messages' => [
+//             ['type' => 'text', 
+//              'text' => $text]
+//          ]
+//       ];
+//       $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+//       $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+//       echo "Result: ".$send_result."\r\n";
+//    }
+// }
+
 if ( sizeof($request_array['events']) > 0 ) {
-      foreach ($request_array['events'] as $event) {
+   foreach ($request_array['events'] as $event) {
       
       $reply_message = '';
       $reply_token = $event['replyToken'];
+      $text = $event['message']['text'];
       $data = [
          'replyToken' => $reply_token,
-         'messages' => [
-            ['type' => 'text', 
-             'text' => $text]
-         ]
+         'messages' => [['type' => 'text', 'text' => $text ]]
       ];
       $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-      $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+      $send_result = send_reply_message($API_URL.'/reply',      $POST_HEADER, $post_body);
       echo "Result: ".$send_result."\r\n";
-   }
+    }
 }
 echo "OK";
 
